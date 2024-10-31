6c1656f072d342c6f34a7cad5166f7fb838722a6
Park JunYong <dyden119@gmail.com>
2024-10-31 21:38:54 +0900

    chore: api호출 에러 핸들링

--- api/api.ts
+++ api/api.ts
@@ -2,24 +2,41 @@ import axios from "axios";
 
 const BASE_URL = "https://learn.codeit.kr/api/";
 
+type ErrorType = {
+  response: {
+    data: string;
+    status: number;
+  };
+};
+
 export const instance = axios.create({
   timeout: 5000,
   baseURL: BASE_URL,
 });
 
 export async function getSurveys(limit: number) {
-  const response = await instance.get(
-    `${BASE_URL}color-surveys?limit=${limit}`
-  );
-  return response.data;
+  try {
+    const response = await instance.get(
+      `${BASE_URL}color-surveys?limit=${limit}`
+    );
+    return response.data;
+  } catch (e) {
+    const Error = e as ErrorType;
+    alert(Error.response.data);
+  }
 }
 
 export async function sendSurvey(mbti: string, colorCode: string) {
-  const response = await instance.post(`${BASE_URL}color-surveys`, {
-    mbti,
-    colorCode,
-    password: "0000",
-  });
-  console.log(response);
-  return response;
+  try {
+    const response = await instance.post(`${BASE_URL}color-surveys`, {
+      mbti,
+      colorCode,
+      password: "1234",
+    });
+    console.log(response);
+    return response;
+  } catch (e) {
+    const Error = e as ErrorType;
+    alert(Error.response.data);
+  }
 }
